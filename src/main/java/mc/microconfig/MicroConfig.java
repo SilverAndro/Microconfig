43bbc06eb872cc59ca5a4b7325fe64bf2f9dc720
P03W <P03W@users.noreply.github.com>
2021-03-08 00:56:31 -0500

    Docs, optimisations, comments, and a method that doesnt depend on fabric loader

--- src/main/java/mc/microconfig/MicroConfig.java
+++ src/main/java/mc/microconfig/MicroConfig.java
@@ -6,18 +6,46 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.UnknownFormatConversionException;
 
 public class MicroConfig {
+    /**
+     * Takes in a name and creates a file in the config directory as specified by fabric
+     *
+     * Creates the file if it doesn't exist, reads it into the object, then sync the file with the expected structure
+     *
+     * @param name The name of the file, will have ".mcfg" appended
+     * @param configData The data object, must implement ConfigData as a sanity check
+     * @return The original object, with fields set to the read values from the config file
+     */
+    @SuppressWarnings("unused")
     public static <T extends ConfigData> T getOrCreate(String name, T configData) {
+        // Get the expected file
         File configFile = FabricLoader.getInstance().getConfigDir().resolve(name + ".mcfg").toFile();
+        return getOrCreate(configFile, configData);
+    }
+    
+    /**
+     * Works with any file, doesn't depend on FabricLoader
+     *
+     * Creates the file if it doesn't exist, reads it into the object, then sync the file with the expected structure
+     *
+     * @param configFile The file for for the config file
+     * @param configData The data object, must implement ConfigData as a sanity check
+     * @return The original object, with fields set to the read values from the config file
+     */
+    public static <T extends ConfigData> T getOrCreate(File configFile, T configData) {
+        // If it doesn't exist, create it for the first time
         if (!configFile.exists()) {
             createConfigFile(configFile, configData);
         }
+        
+        // Load it back
         loadConfig(configFile, configData);
+        
+        // Try to overwrite the whole file again but with the loaded object
+        // This makes sure the file is always in sync with the expected values
         try {
             FileWriter writer = new FileWriter(configFile, false);
             writer.flush();
@@ -30,12 +58,13 @@ public class MicroConfig {
     }
     
     private static void loadConfig(File configFile, ConfigData configData) {
-        ArrayList<Field> unsetFields = new ArrayList<>(Arrays.asList(configData.getClass().getDeclaredFields()));
-        
         try {
+            // Scan in the file
             Scanner scanner = new Scanner(configFile);
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
+                // Ignore very short lines or comments
+                // 3 characters because the minimum meaningful line is "a=1" or similar
                 if (line.startsWith("//") || line.length() <= 3) {
                     continue;
                 }
@@ -43,6 +72,7 @@ public class MicroConfig {
                 String fieldName;
                 String value;
                 try {
+                    // Attempt to parse the line
                     String[] split = line.split("=");
 
                     fieldName = split[0].replace(" ", "");
@@ -56,27 +86,19 @@ public class MicroConfig {
                     );
                 }
                 
+                // Find the field that matches the expected name
                 Field field = null;
                 for (Field possible : configData.getClass().getFields()) {
                     if (possible.getName().equals(fieldName)) {
                         field = possible;
+                        break;
                     }
                 }
-                unsetFields.remove(field);
                 
-                while (value.startsWith(" ")) {
-                    value = value.substring(1);
-                }
-                
+                // If we found a field, set it
                 if (field != null) {
                     unpackFieldValuePair(configData, field, value);
                 }
-            }
-            
-            for (Field field : unsetFields) {
-                FileWriter writer = new FileWriter(configFile, true);
-                appendDefaultField(writer, configData, field);
-                writer.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -85,6 +107,7 @@ public class MicroConfig {
     
     private static void createConfigFile(File configFile, ConfigData object) {
         try {
+            // Add every field to the file
             FileWriter writer = new FileWriter(configFile);
             for (Field field : object.getClass().getFields()) {
                 appendDefaultField(writer, object, field);
@@ -97,14 +120,19 @@ public class MicroConfig {
     
     private static void appendDefaultField(FileWriter writer, ConfigData type, Field field) {
         try {
+            // Check if the field has a comment annotation
             Comment annotation = field.getAnnotation(Comment.class);
             
+            // If it does, write the comment and mark that we need an extra line
+            // This keeps the output overall more clean
             boolean doExtraBreak = false;
             if (annotation != null) {
                 String clean = "//" + annotation.value().replace("\n", "\n//");
                 writer.append(clean).append("\n");
                 doExtraBreak = true;
             }
+            
+            // Write the pair and extra line if required
             writer.append(field.getName()).append("=").append(field.get(type).toString()).append("\n");
             if (doExtraBreak) {
                 writer.append("\n");
@@ -115,6 +143,8 @@ public class MicroConfig {
     }
     
     private static void unpackFieldValuePair(ConfigData object, Field field, String value) {
+        // Fairly self explanatory
+        // Only with I could use switch blocks
         try {
             Class<?> fieldType = field.getType();
             if (fieldType == int.class) {
